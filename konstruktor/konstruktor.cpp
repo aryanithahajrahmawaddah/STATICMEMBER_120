ca98ebd10bc39af8a4073c561d008ad2e6d40d3f
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-05-15 18:29:42 +1200

    nama sudah dan nim menampilkan angka acak

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -23,5 +23,12 @@ mahasiswa::mahasiswa(int iNim) {
 	nim = iNim;
 }
 
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
 
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
 
